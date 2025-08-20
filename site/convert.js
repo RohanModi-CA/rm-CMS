13587298bf0acb9e0efb623ce923e80fe2b9c82a
Rohan Modi <git@rohanmodi.ca>
2025-08-20 18:28:44 -0400

    fixes, cleanups, misc

--- site/convert.js
+++ site/convert.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", () =>
 	const preview_html_button = document.getElementById("preview-html-button");
 	const html_preview_iframe = document.getElementById("html-preview-iframe");
 	const push_statics_button = document.getElementById("push-statics-button")
-	const destination_textarea = document.getElementById("destination-textarea")
-	const push_html_button = document.getElementById("push_html_button")
+	const destination_input = document.getElementById("destination-input")
+	const push_html_button = document.getElementById("push-html-button")
 
 	
 
@@ -117,7 +117,7 @@ document.addEventListener("DOMContentLoaded", () =>
 			html_preview_iframe_doc.close();
 
 			push_statics_button.classList.remove("hidden");
-			destination_textarea.classList.remove("hidden");
+			destination_input.classList.remove("hidden");
 			push_html_button.classList.remove("hidden")
 			push_html_button.disabled = true
 
@@ -153,7 +153,7 @@ document.addEventListener("DOMContentLoaded", () =>
 		response = await fetch("/push-html", 
 		{
 			method: "POST",
-			body: destination_textarea.value
+			body: destination_input.value
 		});
 
 	});
